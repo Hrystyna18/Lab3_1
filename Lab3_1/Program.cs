e85aefb3baf67133420295f2c9a7737fa657e907
Hrystyna18 <tymkivhrystyna007@icloud.com>
2020-04-27 22:00:50 +0300

    Add project files.

---
+++ Lab3_1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+class stringtest
+{
+    static void Main()
+    {
+        string[] strs;
+        bool doubLetter = false;
+        Console.WriteLine("введiть рядок : ");
+        string s = Console.ReadLine();
+        strs = s.Split(null);
+        Console.WriteLine("к-сть слiв : " + s.Length);
+        for (int j = 0; j < strs.Length; j++)
+            if (strs[j].Length > 1)
+                for (int i = 1; i < strs[j].Length; i++)
+                    if (strs[j][i] == strs[j][i - 1])
+                    {
+                        Console.WriteLine("слово з подвоєнням : " + strs[j]);
+                        strs[j] = null;
+                        doubLetter = true;
+                        break;
+                    }
+        if (!doubLetter) Console.WriteLine("немає слiв з подвоєнням");
+        foreach (string word in strs) Console.Write(word + " ");
+        Console.WriteLine();
+    }
+}
